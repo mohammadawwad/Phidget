98c550e5acf1de3f3378affdb21a18b7ed2bb50c
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2022-05-27 09:53:36 -0700

    got led now working with the game its almost finished

--- Java/WhackAMole.java
+++ Java/WhackAMole.java
@@ -19,28 +19,24 @@ public class WhackAMole {
     private static long timeOn;
 
     private static long number = 4000;
-    private static int value;
-    private static int y;
-    
     //used to check if the game has ended
     private static boolean continuePlaying = true;
     private static int roundCounter = 0;
+    private static int missedCounter = 0;
 
 
 
     //Handle Exceptions
     public static void main(String[] args) throws Exception {
 
+        //player score
         int score = 0;
-
 
         //Create
         DigitalInput redButton = new DigitalInput();
         DigitalOutput redLED = new DigitalOutput();
         DigitalInput greenButton = new DigitalInput();
         DigitalOutput greenLED = new DigitalOutput();
-
-
 
         //Address
         redButton.setHubPort(0);
@@ -52,8 +48,6 @@ public class WhackAMole {
         greenLED.setHubPort(4);
         greenLED.setIsHubPortDevice(true);
 
-
-
         //Open
         redButton.open(1000);
         redLED.open(1000);
@@ -64,33 +58,73 @@ public class WhackAMole {
 
         System.out.println("Welcome to Whack A Mole!");
         System.out.println("Make sure to hold down the button until the light turns off!");
-
-
-
+        
+        
+        //Creates and opens lcd screen
+        LCD lcd = new LCD();
+        lcd.open(3000);
+        
+        //intro message
+        lcd.clear();
+        lcd.writeText(LCDFont.DIMENSIONS_6X12, 28, 0, "Whack-A-Mole");
+        lcd.drawLine(27, 10, 99, 10);
+        lcd.writeText(LCDFont.DIMENSIONS_6X10, 45, 25, "Begin!");
+        lcd.flush();
+        
+        //allowing enough time for user reaction before game starts
+        Thread.sleep(3000);
+        
+        
         System.currentTimeMillis();
 
-        //Use your Phidgets
+        //UKeeps playing untill you get 3 wronge 
         while (continuePlaying) {
-            //Randomizer();
-            Thread.sleep(timeOn);
             
+            //Welcome Message on LCD Screen
+            lcd.clear();
+            lcd.writeText(LCDFont.DIMENSIONS_6X12, 28, 0, "Whack-A-Mole");
+            lcd.drawLine(27, 10, 99, 10);
+            
+            //outputting the score on the LCD screen            
+            lcd.writeText(LCDFont.DIMENSIONS_6X12, 32, 25, "Score: " + score);
+            lcd.writeText(LCDFont.DIMENSIONS_6X12, 32, 35, "Missed: " + missedCounter + "/3");
+            lcd.flush();
+
+            //allows enough time for user reaction in the first round
+            if(roundCounter == 0){
+                Thread.sleep(1000);
+            }
+            
+            //ends game if you missed 3 rounds
+            if(missedCounter >= 3){
+                continuePlaying = false;
+                System.out.println("Game Over!");
+                lcd.writeText(LCDFont.DIMENSIONS_6X12, 32, 45, "Game Over!");
+                lcd.flush();
+            }
+            
+            Thread.sleep(timeOn);
             roundCounter++;
 
             //decides which light will turn on
             lightDecider = RAND.nextInt(2) + 1;
 
-
             //decrements the time the light is on for
             timeDecrementer();
             timeOn = number;
+         
 
+            //turns on and off the leds
             if (timeOn > 0){
             switch(lightDecider) {
+                
+                //green lights ON
                 case 1:
                     greenLED.setState(true);
 
                     boolean reset = false;
                     
+                    //keeps led on for certain time based on math function
                     for (long stop = System.currentTimeMillis() + 1000; stop > System.currentTimeMillis();) {
                         if(greenButton.getState()){
                             score++;
@@ -99,8 +133,8 @@ public class WhackAMole {
                             break;
                             
                         } else if (redButton.getState()) {
-                            score = 0;
-                            System.out.println("Oh no! Wrong button! Score: " + score);
+                            reset = true;
+                            System.out.println("Oh no! Wrong button!");
                             break;
                         } else{
                             reset = true;
@@ -108,22 +142,23 @@ public class WhackAMole {
                         
                     }
 
+                    //setting that you have missed a LED
                     if(reset == true){
-                        score = 0;
                         System.out.println("Score Reset: " + score);
+                        missedCounter++;
                     }
                     
+                    //turns off led
                     greenLED.setState(false);
+                    break;
 
-
-                break;
-
+                //red light tunrs ON
                 case 2:
                     redLED.setState(true);
                     
                     boolean reset2 = false;
                     
-                    
+                    //keeps led on for certain time based on math function
                     for (long stop = System.currentTimeMillis() + 1000; stop > System.currentTimeMillis();) {
                         if(redButton.getState()){
                             score++;
@@ -132,8 +167,8 @@ public class WhackAMole {
                             break;
                             
                         } else if (greenButton.getState()) {
-                            score = 0;
-                            System.out.println("Oh no! Wrong button! Score: " + score);
+                            reset2 = true;
+                            System.out.println("Oh no! Wrong button!");
                             break;
                         } else{
                             reset2 = true;
@@ -141,18 +176,17 @@ public class WhackAMole {
                         
                     }
 
+                    //setting that you have missed a LED
                     if(reset2 == true){
-                        score = 0;
                         System.out.println("Score Reset: " + score);
+                        missedCounter++;
                     }
                     
-
-
+                    //turns off led
                     redLED.setState(false);
 
                 break;
                 }
-
             }
         }
     }
@@ -161,9 +195,11 @@ public class WhackAMole {
     public static long timeDecrementer(){
 
         //a non linear time function that decreases the time all the way to 0.2 seconds and steadies there
-        number = (long) (((4) * Math.pow(1.3, -roundCounter) + 0.2) * 1000);
+        number = (long) (((4) * Math.pow(1.3, -roundCounter) + 0.15) * 1000);
         System.out.println(number);
                           
         return number;
     }
+    
+    
 }
